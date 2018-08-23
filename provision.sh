85a2fc1b7d9accee18d5003316ad32b2fc77451c
James Tanner <tanner.jc@gmail.com>
2018-08-22 20:50:42 -0400

    Set the version of puppetlabs-mysql to install

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 apt-get install --yes puppet || exit 1
-puppet module install puppetlabs-mysql || exit 1
+puppet module install --version=3.6.2 --ignore-dependencies puppetlabs-mysql || exit 1
 puppet module install puppetlabs-rabbitmq --ignore-dependencies || exit 1
 puppet module install puppetlabs-mongodb || exit 1
